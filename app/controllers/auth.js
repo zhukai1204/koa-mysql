9bd5dd08c258c0f34aed10516421cf68796b6c7b
luck <515269703@qq.com>
2018-02-27 10:37:29 +0800

    update

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,5 +1,8 @@
+const request = require('request');
+import superagent from 'superagent';
 const xiaoMi = async (ctx, next) => {
-  
+  let url = `https://account.xiaomi.com/pass/serviceLoginAuth2?_dc=${Date.now()}`
+  ctx.body = 'xiaoMi';
 };
 
 export default {
